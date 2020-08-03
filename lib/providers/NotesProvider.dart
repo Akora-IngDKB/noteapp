8d7af94461d51ee79206926d9a67832402eea0ed
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-03 13:08:50 +0100

    Update

--- lib/providers/NotesProvider.dart
+++ lib/providers/NotesProvider.dart
@@ -3,14 +3,16 @@ import 'package:note/providers/BaseProvider.dart';
 
 class NotesProvider extends BaseProvider {
   List<Note> _notesList = [];
-  
+
   List<Note> get notes => _notesList;
 
   void addNote(Note n) {
     _notesList.add(n);
     notifyListeners();
   }
- 
 
-
+  void deleteNote(Note n) {
+    _notesList.remove(n);
+    notifyListeners();
+  }
 }
