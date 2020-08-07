a5603c49bd55a23c80c882dcf62d110e5bafb594
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-07 18:52:07 +0100

    Update

--- lib/widgets/HomeNoteItem.dart
+++ lib/widgets/HomeNoteItem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note/models/Note.dart';
+import 'package:note/screens/Workspace.dart';
 
 class HomeNoteItem extends StatefulWidget {
   final Note note;
@@ -12,36 +13,46 @@ class HomeNoteItem extends StatefulWidget {
 class _HomeNoteItemState extends State<HomeNoteItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      height: double.infinity,
-      decoration: BoxDecoration(
-        color: Theme.of(context).unselectedWidgetColor.withOpacity(0.3),
-        border: Border.all(color: Theme.of(context).disabledColor),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            widget.note.title,
-            style: TextStyle(
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).primaryColor,
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => WorkSpace(existingNote: widget.note),
           ),
-          SizedBox(height: 10),
-          Text(
-            widget.note.text,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 6,
-            style: TextStyle(
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        height: double.infinity,
+        decoration: BoxDecoration(
+          color: Theme.of(context).unselectedWidgetColor.withOpacity(0.3),
+          border: Border.all(color: Theme.of(context).disabledColor),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              widget.note.title,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
                 color: Theme.of(context).primaryColor,
-                height: 1.3,
-                fontSize: 13),
-          ),
-        ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              widget.note.text,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 6,
+              style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  height: 1.3,
+                  fontSize: 13),
+            ),
+          ],
+        ),
       ),
     );
   }
