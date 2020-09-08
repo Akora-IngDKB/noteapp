071620cccd7f4fef65b8aadc909786e8b6a65e43
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-09-08 16:10:15 +0100

    Update

--- lib/screens/Search.dart
+++ lib/screens/Search.dart
@@ -8,33 +8,39 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                disabledBorder: InputBorder.none,
-                filled: true,
-                fillColor: Theme.of(context).unselectedWidgetColor,
-                contentPadding: EdgeInsets.only(left: 20),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Icon(Icons.search, color: Colors.grey, size: 15),
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                hintText: 'Search notes',
-                hintStyle: TextStyle(
-                  fontSize: 17,
-                  color: Colors.grey,
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 30, top: 60),
+                child: TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    disabledBorder: InputBorder.none,
+                    filled: true,
+                    fillColor: Theme.of(context).unselectedWidgetColor,
+                    contentPadding: EdgeInsets.only(left: 20),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Icon(Icons.search, color: Colors.grey, size: 15),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Search notes',
+                    hintStyle: TextStyle(
+                      fontSize: 17,
+                      color: Colors.grey,
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
