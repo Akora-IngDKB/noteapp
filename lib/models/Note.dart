87a650cba45d6323c1ccc79f27233a42c318eb69
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-10-19 17:06:27 +0100

    Added json functions

--- lib/models/Note.dart
+++ lib/models/Note.dart
@@ -6,4 +6,18 @@ class Note {
     this.title = '',
     this.text = '',
   });
+
+  factory Note.fromJSON(Map<String, dynamic> json) {
+    return new Note(
+      title: json["title"],
+      text: json["text"],
+    );
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {
+      "title": title,
+      "content": text,
+    };
+  }
 }
