5fe445f62cb3e1a1c440841b59f49d19c57bd5c6
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-10-24 13:59:48 +0100

    Update

--- lib/screens/Workspace.dart
+++ lib/screens/Workspace.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:note/models/Note.dart';
 import 'package:note/providers/NotesProvider.dart';
+import 'package:note/screens/Speech_to_text.dart';
 import 'package:note/screens/home.dart';
 import 'package:provider/provider.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
@@ -16,11 +17,13 @@ class WorkSpace extends StatefulWidget {
 }
 
 class _WorkSpaceState extends State<WorkSpace> {
+  DateTime now = DateTime.now();
   bool isBold = false;
   bool isItalic = false;
   bool isUnderlined = false;
   bool isRecording = false;
   Note note = Note();
+  Note date;
   bool isChanged = false;
   stt.SpeechToText _speech;
 
@@ -108,7 +111,9 @@ class _WorkSpaceState extends State<WorkSpace> {
       if (available) {
         setState(() => isRecording = true);
         _speech.listen(onResult: (val) {
-          note.text = val.recognizedWords;
+          setState(() {
+            note.text = val.recognizedWords;
+          });
         });
       }
     } else {
@@ -122,137 +127,173 @@ class _WorkSpaceState extends State<WorkSpace> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      appBar: AppBar(
-        elevation: 0,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: Theme.of(context).primaryColor,
-            size: 20,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Theme.of(context).primaryColor,
+              size: 20,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: <Widget>[
-          Tooltip(
-            message: 'Delete',
-            child: IconButton(
-                disabledColor: Colors.grey,
-                icon: Icon(CupertinoIcons.delete_solid),
-                color: Theme.of(context).primaryColor,
-                onPressed: () async {
-                  if (note.title.isNotEmpty || note.text.isNotEmpty) {
-                    final delete = await _showDialog();
-                    if (delete ?? false) Navigator.pop(context);
-                  }
-                }),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: FlatButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              color: Colors.blue,
-              child: Text(
-                "Save",
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                if (note.text.isEmpty) {
-                  return showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: Text('You can\'t save an empty note'),
-                      actions: [
-                        FlatButton(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+          actions: <Widget>[
+            Tooltip(
+              message: 'Delete',
+              child: IconButton(
+                  disabledColor: Colors.grey,
+                  icon: Icon(CupertinoIcons.delete_solid),
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () async {
+                    if (note.title.isNotEmpty || note.text.isNotEmpty) {
+                      final delete = await _showDialog();
+                      if (delete ?? false) Navigator.pop(context);
+                    }
+                  }),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                color: Colors.blue,
+                child: Text(
+                  "Save",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  if (note.text.isEmpty) {
+                    return showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text('You can\'t save an empty note'),
+                        actions: [
+                          FlatButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Cancel'),
                           ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text('Cancel'),
-                        ),
-                      ],
-                    ),
-                  );
-                } else if (widget.existingNote != null) {
-                  provider.update(widget.existingNote, note);
-                } else {
-                  provider.addNote(note);
-                }
-                Navigator.pop(context);
-              },
-            ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onChanged: (val) {
-                isChanged = !isChanged;
-                note.title = val;
-              },
-              controller: TextEditingController(text: note.title.trim()),
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 23,
-                fontWeight: FontWeight.w600,
-              ),
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                hintText: 'Title',
-                hintStyle: TextStyle(
-                  fontSize: 19,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.normal,
-                ),
-                border: InputBorder.none,
-                enabledBorder: InputBorder.none,
+                        ],
+                      ),
+                    );
+                  } else if (widget.existingNote != null) {
+                    provider.update(widget.existingNote, note);
+                  } else {
+                    provider.addNote(note);
+                  }
+                  Navigator.pop(context);
+                },
               ),
             ),
-            TextField(
-              onChanged: (val) {
-                note.text = val;
-                isChanged = !isChanged;
-              },
-              controller: TextEditingController(text: note.text.trim()),
-              style: TextStyle(
-                fontSize: 17,
-                color: Theme.of(context).primaryColor,
-              ),
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                hintText: 'Type here',
-                hintStyle: TextStyle(
-                  fontSize: 19,
-                  color: Colors.grey,
-                ),
-                border: InputBorder.none,
-                enabledBorder: InputBorder.none,
-              ),
-            ),
-            SizedBox(height: 50),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: record,
-        child: Image.asset(
-          isRecording ? 'images/record.gif' : 'images/microphone.png',
-          color: Colors.white,
-          width: 18,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                onChanged: (val) {
+                  isChanged = !isChanged;
+                  note.title = val;
+                  note.date = DateTime.now();
+                },
+                controller: TextEditingController(text: note.title.trim()),
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 23,
+                  fontWeight: FontWeight.w600,
+                ),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  hintText: 'Title',
+                  hintStyle: TextStyle(
+                    fontSize: 19,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                ),
+              ),
+              TextField(
+                onChanged: (val) {
+                  note.text = val;
+                  isChanged = !isChanged;
+                  note.date = DateTime.now();
+                },
+                controller: TextEditingController(text: note.text.trim()),
+                style: TextStyle(
+                  fontSize: 17,
+                  color: Theme.of(context).primaryColor,
+                ),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  hintText: 'Type here',
+                  hintStyle: TextStyle(
+                    fontSize: 19,
+                    color: Colors.grey,
+                  ),
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                ),
+              ),
+              SizedBox(height: 50),
+            ],
+          ),
         ),
-      ),
-    );
+        bottomSheet: Container(
+          padding: const EdgeInsets.only(left: 20),
+          height: 50,
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: Row(
+            children: [
+              Text(
+                note.date == null
+                    ? 'Created at ${now.day}-${now.month}-${now.year} , ${now.hour}:${now.minute} '
+                    : 'Last Edited : ${note.date.day}/${note.date.month}/${note.date.year}  ${note.date.hour}:${note.date.minute}',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: IconButton(
+                  icon: Image.asset(
+                    'images/camera_icon.png',
+                    width: 25,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: IconButton(
+                  icon: Image.asset(
+                    'images/mic_icon.png',
+                    width: 18,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return SpeechScreen();
+                    }));
+                  },
+                ),
+              )
+            ],
+          ),
+        ));
   }
 
   Widget buildFormatButtons(BuildContext context) {
