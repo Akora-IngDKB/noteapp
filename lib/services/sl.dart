e2cabbd69d0b94183b3db173eb886736fbcc3d53
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-10-27 10:49:49 +0000

    Registered setup locator with GetIt

--- lib/services/sl.dart
+++ lib/services/sl.dart
@@ -1,8 +1,9 @@
 import 'package:get_it/get_it.dart';
+import 'package:note/services/FileContract.dart';
 import 'package:note/services/FileService.dart';
 
 GetIt sl = GetIt.instance;
 
 setupServiceLocator() {
-  sl.registerLazySingleton<FileService>(() => FileService());
+  sl.registerLazySingleton<FileContract>(() => FileService());
 }
